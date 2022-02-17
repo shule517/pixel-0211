4832d9c0bf2743ec730e5cc5b6b2dd47de91cd68
シュール <shule517@gmail.com>
2022-02-17 21:15:30 +0900

    ネットから画像をダウンロードして表示する

---
+++ Assets/Source/Artwork.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Artwork : MonoBehaviour
+{
+    public string mediaUrl;
+
+    [SerializeField] private RawImage _image;
+
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        Debug.Log("mediaUrl: " + mediaUrl);
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(mediaUrl);
+
+        //画像を取得できるまで待つ
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log("error");
+            Debug.Log(www.error);
+        }
+        else
+        {
+            // //取得した画像のテクスチャをRawImageのテクスチャに張り付ける
+            // _image.texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+
+            Debug.Log("success");
+            var spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+            // spriteRenderer.sprite = sprite;
+
+            var texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+
+            // 新しいスプライトのメッシュタイプをFullRectにして、9スライスに対応できるようにする
+            // 9スライスとはいってもborderはデフォルトの(0, 0, 0, 0)なので、画像全体がサイズに合わせて伸縮することになる
+            Sprite sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 700.0f, 0, SpriteMeshType.FullRect);
+
+
+
+
+            // Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            // Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            spriteRenderer.sprite = sprite;
+
+            // GetComponent<Renderer>().material.mainTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Source/GameManager.cs
+++ Assets/Source/GameManager.cs
@@ -9,9 +9,47 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 20; i++)
+        var artworkUrls = new string[] {
+            "http://pbs.twimg.com/media/FHFaZREaAAIsq8o.png",
+            "http://pbs.twimg.com/media/FLKsamJaQAQgHSA.jpg",
+            "http://pbs.twimg.com/media/FLgBV1kakAAvdtQ.png",
+            "http://pbs.twimg.com/media/EplgCmdVgAcXIau.png",
+            "http://pbs.twimg.com/media/E6kcWAgUUAA2L8H.png",
+            "http://pbs.twimg.com/media/EsgMaq5VEAE2vb8.png",
+            "https://pbs.twimg.com/profile_banners/1334918399739097089/1609562712",
+            "http://pbs.twimg.com/media/E6eB2d9VUAMPqAI.png",
+            "http://pbs.twimg.com/media/Era42A5VoAEpVVa.png",
+            "http://pbs.twimg.com/media/Eqn9ZOmUYAASTo9.png",
+            "http://pbs.twimg.com/media/EqqXAsKU0AAHSSO.png",
+            "http://pbs.twimg.com/media/EpNDHVkU8AQYp4V.png",
+            "http://pbs.twimg.com/media/E3NE-JrVkAkLOMu.png",
+            "http://pbs.twimg.com/media/EsLgAVJUwAI1p_5.png",
+            "http://pbs.twimg.com/media/Ep7MuISVgAEYmgX.png",
+            "http://pbs.twimg.com/media/FGuuYjgXEAMM8Y1.png",
+            "http://pbs.twimg.com/media/E3iDO_HVUAYmlIF.png",
+            "http://pbs.twimg.com/media/Ez_pVluWEAI8e_O.jpg",
+            "http://pbs.twimg.com/media/E1lRCfGUcAEfpGh.png",
+            "http://pbs.twimg.com/media/E5SJoIAVUAMGPcf.png",
+            "http://pbs.twimg.com/media/FKVgMohaUAEF-4J.png",
+            "http://pbs.twimg.com/media/FEy1JAfaMAAGrb_.png",
+            "http://pbs.twimg.com/media/EzvcBLLVgAUe2_1.png",
+            "http://pbs.twimg.com/media/FC8vz-baAAAY-DM.jpg",
+            "http://pbs.twimg.com/media/E884elSVcBwQNlR.jpg",
+            
+             };
+
+
+        // for (int i = 0; i < 20; i++)
+
+        var i = -1;
+        foreach (var artworkUrl in artworkUrls)
         {
-            Instantiate(artwork, new Vector3(26 + i * 8, 1.71f, 0), Quaternion.identity);
+            var clone = Instantiate(artwork, new Vector3(i * 8, 1.71f, 0), Quaternion.identity);
+            var obj = clone.GetComponent<Artwork>();
+            obj.mediaUrl = artworkUrl;
+            // var clone = Instantiate(artwork, new Vector3(26 + i * 8, 1.71f, 0), Quaternion.identity);
+            // clone.LoadImage("http://pbs.twimg.com/media/FHFaZREaAAIsq8o.png");
+            i++;
         }
     }
 
