c95696c50a3e17b0ec3d26fb2a6b65ee8970ac70
shule517 <shule517@gmail.com>
2022-12-18 11:12:46 +0900

    シーンを整理した

--- Assets/Scripts/ScenarioOpening.cs
+++ Assets/Scripts/ScenarioOpening.cs
@@ -1,5 +1,9 @@
+using DG.Tweening;
+using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
 
 public class ScenarioOpening : MonoBehaviour
 {
@@ -8,7 +12,17 @@ public class ScenarioOpening : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        light2D.intensity = 10;
+        // Å‰‚Í^‚ÁˆÃ
+        light2D.intensity = 0;
+        Sequence moveSequence = DOTween.Sequence();
+
+        // –Ú‚ð‚ ‚¯‚½
+        moveSequence.Append(DOTween.To(() => 0, (float x) => light2D.intensity = x, 1f, 5f).SetDelay(2.0f).SetEase(Ease.InQuad));
+        // Œõ‚Å”ò‚Î‚·
+        moveSequence.Append(DOTween.To(() => 1, (float x) => light2D.intensity = x, 10f, 5f).SetDelay(3.0f));
+
+        // ŽÐ’{ƒV[ƒ“‚ÖØ‚è‘Ö‚¦
+        moveSequence.OnComplete(() => SceneManager.LoadScene("ŽÐ’{Scene"));
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/ScenarioTitle.cs
@@ -0,0 +1,34 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
+
+public class ScenarioTitle : MonoBehaviour
+{
+    public Light2D light2D;
+
+    public float interval = 2.5f;
+    public float maxIntensity = 1.5f;
+    public float minIntensity = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sequence moveSequence = DOTween.Sequence();
+
+        // Œõ‚ð‚Ó‚í‚Ó‚í
+        moveSequence.Append(DOTween.To(() => minIntensity, (float x) => light2D.intensity = x, maxIntensity, interval).SetEase(Ease.InOutQuad));
+        moveSequence.Append(DOTween.To(() => maxIntensity, (float x) => light2D.intensity = x, minIntensity, interval).SetEase(Ease.InOutQuad));
+        moveSequence.SetDelay(0.5f);
+        moveSequence.SetLoops(-1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("OpeningScene");
+        }
+    }
+}
