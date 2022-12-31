c5abda202f3f6c4409c3a4d7e1ad4ca0c8d7d432
shota.suzuki <shule517@gmail.com>
2022-12-31 23:18:08 +0900

    電車のドアを開ける

--- Assets/Scripts/Scenario駅ホーム.cs
+++ Assets/Scripts/Scenario駅ホーム.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,9 +8,16 @@ public class Scenario駅ホーム : MonoBehaviour
     public float minX;
     public float maxX;
 
+    //public GameObject train;
+    public GameObject doorLeft;
+    public GameObject doorRight;
+
     // Start is called before the first frame update
     void Start()
     {
+        //train.transform.DOMove(new Vector3(140f, 0.88f, 0f), 10f).SetEase(Ease.OutQuad);
+        doorLeft.transform.DOLocalMove(new Vector3(-0.18f, 0f, 0f), 2f).SetDelay(11f).SetEase(Ease.OutQuad);
+        doorRight.transform.DOLocalMove(new Vector3(+0.18f, 0f, 0f), 2f).SetDelay(11f).SetEase(Ease.OutQuad);
     }
 
     // Update is called once per frame
@@ -21,7 +29,7 @@ public class Scenario駅ホーム : MonoBehaviour
         }
         else if (maxX < player.transform.position.x)
         {
-            SceneManager.LoadScene("美浜Scene");
+            SceneManager.LoadScene("海の駅Scene");
         }
     }
 }
